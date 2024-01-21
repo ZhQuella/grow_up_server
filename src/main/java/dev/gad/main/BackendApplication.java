2f10d7eee9240bf230ee467d58f15d5884dffac7
liangqi <watsonl@qq.com>
2024-01-21 21:34:07 +0800

    feat: add mybatis-flex

--- src/main/java/dev/gad/main/BackendApplication.java
+++ src/main/java/dev/gad/main/BackendApplication.java
@@ -1,10 +1,12 @@
-package dev.gad.utils;
+package dev.gad.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-  public class BackendApplication {
+@MapperScan("dev.gad.main")
+public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
