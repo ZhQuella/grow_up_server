9f2c570d2d9a16f5df46fcc881b4afe285af36ae
liangqi <watsonl@qq.com>
2024-01-21 21:12:19 +0800

    feat: add mybatis-flex

--- src/test/java/dev/gad/utils/BackendApplicationTests.java
+++ src/test/java/dev/gad/utils/BackendApplicationTests.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+public class BackendApplicationTests {
 
 	void contextLoads() {
 	}
 
+	@Test
+	public void t() {
+
+	}
 }
